21108f07e81f54401b7a4cb54690e0594a1a6fd7
milad88884 <interceptors8888@gmail.com>
2023-04-02 18:22:01 +0200

    validation bug fixed

--- src/components/AddUserForm.tsx
+++ src/components/AddUserForm.tsx
@@ -14,21 +14,6 @@ interface PropTypes {
 }
 
 const AddUserForm = ({ onSubmit }: PropTypes) => {
-  const validation = (values: any) => {
-    const errors: any = {};
-    if (values.firstName === "admin") {
-      errors.firstName = {
-        message: "Nice try",
-      };
-    }
-    if (values.score === "") {
-      errors.score = {
-        message: "Nice try",
-      };
-    }
-    return errors;
-  };
-
   return (
     <div className="mt-2">
       <H5 className="mb-1">Add new user or edit existing user's scores</H5>
@@ -38,11 +23,15 @@ const AddUserForm = ({ onSubmit }: PropTypes) => {
         formSettings={{
           mode: "onSubmit",
         }}
-        // validate={validation}
       >
         <HStack alignItems="end" w="600px">
           <TextField name="name" label="User Name" isRequired={true} />
-          <NumberInput name="score" isRequired={true} />
+          <NumberInput
+            placeholder="score"
+            min={1}
+            name="score"
+            isRequired={true}
+          />
           <Button type="submit" variant="success" w="400px">
             Validate
           </Button>
